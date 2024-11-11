de9434b902ad8a4a1805d94f95ab644592b3171f
paper-masterr <diegopapiermeister@gmail.com>
2024-11-10 21:26:19 -0600

    Response with /echo/ endpoint

--- server.c
+++ server.c
@@ -1,3 +1,10 @@
+/**
+ * Author: Diego Papier-Meister
+ * This is the server side of my HTTP Server that I built from scratch.
+ * It was my introductory project to C and I am really proud of how it turned out!
+ * Refer to the README file for any instructions.
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/socket.h>
@@ -6,6 +13,8 @@
 #include <string.h>
 #include <errno.h>
 #include <unistd.h>
+
+#define PORT 4221
 
 int main() {
 
@@ -38,7 +47,7 @@ int main() {
 	
 	// Config socket
 	struct sockaddr_in server_addr = { .sin_family = AF_INET ,
-									 .sin_port = htons(4221),
+									 .sin_port = htons(PORT),
 									 .sin_addr = { htonl(INADDR_ANY) },
 									};
 	
@@ -71,24 +80,50 @@ int main() {
 	 * recv() gets the data on the client_fd socket and stores it in the readBuffer buffer.
 	 * If successful, returns the length of the datagram in bytes,
 	 * otherwise returns -1.
+	 * 
+	 * A buffer is the temporary data storage area where we can use it to read the path that the user is trying to get to.
 	 */
 	char readBuffer[1024];
 	int bytesRecieved = recv(client_fd, readBuffer, sizeof(readBuffer), 0);
 
-	// Extract the path
-	char* reqPath = strtok(readBuffer, " ");
+	if (bytesRecieved == -1) {
+		printf("Recieving error: %s \n", strerror(errno));
+		return 1;
+	}
+
+	// Extract the path == GET /something/idk HTTP/1.1...
+	char *reqPath = strtok(readBuffer, " ");
 	reqPath = strtok(NULL, " ");
+
+	char* content = strtok(NULL, "");
 
 	int bytesSent;
 	
+	/**
+	 * Taking the path that you enter and making endpoints
+	 * strcmp() is a comparing method pretty straightforward
+	 * 
+	 */
 	if (strcmp(reqPath, "/") == 0) {
 		char *response_200 = "HTTP/1.1 200 OK\r\n\r\n";
 		bytesSent = send(client_fd, response_200, strlen(response_200), 0);
+	}
+	else if (strncmp(reqPath, "/echo/", 6) == 0) {
+		reqPath = strtok(reqPath, "/");
+		reqPath = strtok(NULL, "");
+
+		int contentLength = strlen(reqPath);
+		char response[512];
+
+		sprintf(response, "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", contentLength, reqPath);
+		printf("Echoing: %s\n", response);
+		bytesSent = send(client_fd, response, strlen(response), 0);
 	}
 	else {
 		char *response_404 = "HTTP/1.1 404 Not Found\r\n\r\n";
 		bytesSent = send(client_fd, response_404, strlen(response_404), 0);
 	}
+
 	if(bytesSent < 0) {
 		printf("Send failed");
 		return 1;
